cd0a5fe9a4f9c1d96f8b73a79f8ce7fef3f65211
DominicWoo <w35703544@gmail.com>
2019-08-31 18:15:28 +0800

    Add files via upload

--- clean_expired_sessions.py
+++ clean_expired_sessions.py
@@ -8,12 +8,26 @@ mydbuser = os.getenv('MONGODB_USER')
 mydbpassword = os.getenv('MONGODB_PASSWORD')
 mydbname = os.getenv('DB')
 mydblink = os.getenv('MONGODB_LINK')
-mydbport = os.getenv('PORT')
-mongodb = "mongodb://" + mydbuser + ':' + mydbpassword + '@' + mydblink + ':' + mydbport + "/"
+mydbport = os.getenv('MONGODB_PORT')
 
-myclient = pymongo.MongoClient(mongodb)
-mydb = myclient[mydbname]
-mycol = mydb["session"]
-myquery = {"expiration": {"$lt": datetime.datetime.today()}}
-n = mycol.delete_many(myquery)
-print(n.deleted_count, "个session已清理")
+try:
+    if mydbuser is None:
+        if mydbpassword is None:
+            mongodb = "mongodb://" + mydblink + ':' + mydbport + "/"
+        else:
+            mongodb = "mongodb://" + mydbuser + '@' + mydblink + ':' + mydbport + "/"
+    else:
+        mongodb = "mongodb://" + mydbuser + ':' + mydbpassword + '@' + mydblink + ':' + mydbport + "/"
+except TypeError:
+    print("请先配置MongoDB连接地址！")
+
+try:
+    myclient = pymongo.MongoClient(mongodb)
+    mydb = myclient[mydbname]
+    mycol = mydb["session"]
+    myquery = {"expiration": {"$lt": datetime.datetime.today()}}
+    n = mycol.delete_many(myquery)
+except TypeError, NameError:
+    print("连接失败!")
+else:
+    print(n.deleted_count, "个session已清理")
