2c1ecf10726a68d1ac06ed05fcf2970acc60d7da
DominicWoo <w35703544@gmail.com>
2019-09-01 15:01:51 +0800

    Add files via upload

--- clean_expired_sessions.py
+++ clean_expired_sessions.py
@@ -3,39 +3,37 @@
 import pymongo
 import datetime
 import os
+import re
 
 username = os.getenv('MONGODB_USER')
 password = os.getenv('MONGODB_PASSWORD')
 db_name = os.getenv('DB')
 host = os.getenv('MONGODB_HOST')
 port = os.getenv('MONGODB_PORT')
+expiration = os.getenv('SESSION_EXPIRATION')
 
 print(f"Current time: {datetime.datetime.now()}")
 
 try:
     if username is None or username == '':
-        if password is None or password == '':
-            mongodb = "mongodb://" + host + ':' + port + "/"
-        else:
-            mongodb = "mongodb://" + username + '@' + host + ':' + port + "/"
+        mongodb = "mongodb://" + host + ':' + port + "/"
     else:
         mongodb = "mongodb://" + username + ':' + password + '@' + host + ':' + port + "/"
 except TypeError:
     print("请先配置MongoDB连接地址！")
 
-try:
-    client = pymongo.MongoClient(mongodb)
-    db = client[db_name]
-    col = db["session"]
-    query = {"expiration": {"$lt": datetime.datetime.today()}}
-    n = col.delete_many(query)
-except (TypeError, NameError, pymongo.errors.ServerSelectionTimeoutError):
-    print("连接失败!")
-except ValueError:
-    print("端口值有误!")
-except pymongo.errors.OperationFailure:
-    print("验证失败!")
-except pymongo.errors.ConfigurationError:
-    print("请配置密码!")
-else:
-    print(n.deleted_count, "个session(s)已清理")
+
+client = pymongo.MongoClient(mongodb)
+db = client[db_name]
+col = db["session"]
+
+days = re.compile(r'\d+d',re.I)
+hours = re.compile(r'\d+h',re.I)
+num = re.compile(r'\d+')
+now = datetime.datetime.today()
+d = int(num.search(days.search(expiration).group(0)).group(0))
+h = int(num.search(hours.search(expiration).group(0)).group(0))
+query = {"expiration": {"$lt": (now + datetime.timedelta(days=d,hours=h))}}
+
+n = col.delete_many(query)
+print(n.deleted_count, "个session(s)已清理")
